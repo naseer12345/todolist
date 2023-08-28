8ab8619c189bcf643987c8e4dd9f9ff7a62066ee
naseer12345 <naseer.khan00128@gmail.com>
2023-08-28 10:50:46 +0100

    todo list without the css for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [inputText, setInputText] = useState('');
+
+  const addItem = () => {
+    if (inputText) {
+      setItems(prevItems => [...prevItems, inputText]);
+      setInputText(''); 
+    }
+  };
+
+  const deleteItem = (indexToDelete) => {
+    setItems(prevItems => prevItems.filter((_, index) => index !== indexToDelete));
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button onClick={addItem}>Add Item</button>
+
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteItem(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
